d377580aabaaef9a44d145350bfe868463f68ff5
taz <tsk.nakamura@gmail.com>
2020-11-28 11:00:09 +0900

    hello manticore

---
+++ contracts/HelloManticore.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6;
+
+// manticore contracts/HelloManticore.sol
+contract HelloManticore {
+    function add(uint a, uint b) external pure returns (uint) {
+        uint c = a + b;
+        require(c >= a, "c < a");
+        require(c >= b, "c < b");
+        // c = a + b and c >= a and c >= b and (c < a or c < b)
+        return c;
+    }
+}
---
+++ contracts/hello_manticore.py
@@ -0,0 +1,33 @@
+# python3 contracts/hello_manticore.py
+import os
+from manticore.ethereum import ManticoreEVM
+
+m = ManticoreEVM()
+
+ETHER = 10**18
+
+CONTRACT_DIR = os.path.dirname(os.path.realpath(__file__))
+CONTRACT_PATH = os.path.join(CONTRACT_DIR, "HelloManticore.sol")
+
+user_account = m.create_account(balance=1000*ETHER)
+with open(CONTRACT_PATH) as f:
+    contract = m.solidity_create_contract(f, owner=user_account)
+
+### explore all functions ###
+# symbolic_value = m.make_symbolic_value()
+# symbolic_data = m.make_symbolic_buffer(320)
+
+# m.transaction(
+#     caller=user_account,
+#     address=contract.address,
+#     data=symbolic_data,
+#     value=symbolic_value
+# )
+
+### explore a function ###
+x = m.make_symbolic_value()
+y = m.make_symbolic_value()
+contract.add(x, y, caller=user_account, value=0)
+
+print("Results are in {}".format(m.workspace))
+m.finalize() # stop the exploration
