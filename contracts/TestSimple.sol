ce0b1d899b966c3b9d784c95da50609dbfd6387a
taz <tsk.nakamura@gmail.com>
2020-12-04 09:30:24 +0900

    manticore verifier test simple

--- contracts/TestSimple.sol
+++ contracts/TestSimple.sol
@@ -2,9 +2,18 @@ pragma solidity ^0.6;
 
 import "./Simple.sol";
 
-// echidna-test contracts/TestSimple.sol --contract TestSimple
 contract TestSimple is Simple {
+    /*
+    echidna-test contracts/TestSimple.sol --contract TestSimple
+    */
     function echidna_test_x() external view returns (bool) {
         return x == 123;
     }
+
+    /*
+    manticore-verifier contracts/TestSimple.sol --name TestSimple
+    */
+    // function crytic_test_x() external view returns (bool) {
+    //     return x == 123;
+    // }
 }
